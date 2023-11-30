0fea1a86196e8df0e9ca2cf969603037b90dff30
Jonas <contact@jonascosta.ch>
2023-11-30 16:16:52 +0100

    Now it's REALLY over

--- ts/components/map/MapView.tsx
+++ ts/components/map/MapView.tsx
@@ -1,11 +1,38 @@
-import { ScrollView, Text } from "react-native";
+import { useContext } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { LocalizerContext, LanguageContext, ThemeContext } from "../../App";
 
 const MapView = () => {
+    const localizer = useContext(LocalizerContext);
+    const language = useContext(LanguageContext);
+    const theme = useContext(ThemeContext);
+
     return (
-        <ScrollView>
-            <Text>Map</Text>
+        <ScrollView style={[theme.styles.surface]}>
+            <View style={styles.container}>
+                <Icon
+                    name="construction"
+                    style={theme.styles.onSurface}
+                    size={40}
+                />
+                <Text style={[theme.styles.onSurface, styles.text]}>{localizer.get("construction", language)}</Text>
+            </View>
         </ScrollView>
     )
 };
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        gap: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+    },
+    text: {
+        fontSize: 30,
+    }
+});
 
 export default MapView;
